b3c5336e978b9fef9a9b2205d662e001fbec560b
Jacob Pargin <hello@jacobpgn.com>
2020-11-07 19:31:40 +0000

    Add a basic test for adding tasks

--- src/__tests__/add.test.ts
+++ src/__tests__/add.test.ts
@@ -1,5 +1,31 @@
+import { withApoq, pgPool, clearDatabase } from "./helpers"
+
 describe("add", () => {
-  it("runs a test", async () => {
-    expect(1).toBe(1)
+  beforeEach(async () => {
+    await clearDatabase()
+  })
+
+  it("enqueues a task", async () => {
+    const initialTaskCount = await pgPool().query(
+      `SELECT * FROM apoq_tasks WHERE type = 'addTaskTest';`
+    )
+    expect(initialTaskCount.rows.length).toBe(0)
+
+    const result = await withApoq((apoq) =>
+      apoq.add("addTaskTest", { hello: "world" })
+    )
+
+    expect(result).toMatchObject({ id: expect.any(String) })
+
+    const newTask = await pgPool().query(
+      `SELECT * FROM apoq_tasks WHERE id = $1;`,
+      [result.id]
+    )
+    expect(newTask.rows[0]).toMatchObject({
+      type: "addTaskTest",
+      id: result.id,
+      data: { hello: "world" },
+      state: "pending",
+    })
   })
 })
