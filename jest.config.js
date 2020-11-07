c727e38990fd4d5e90f4ca51acbe46b699a0ab1c
Jacob Pargin <hello@jacobpgn.com>
2020-11-07 19:30:10 +0000

    Add some test helpers

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   maxWorkers: 1,
+  testPathIgnorePatterns: ["<rootDir>/src/__tests__/helpers.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/__tests__/helpers.ts"],
 }
---
+++ src/__tests__/helpers.ts
@@ -0,0 +1,33 @@
+import { Pool } from "pg"
+
+import { Apoq, DEFAULT_TASK_TABLE } from "../"
+
+const TEST_CONNECTION_STRING =
+  process.env.TEST_CONNECTION_STRING || "postgres://localhost/apoq_test"
+
+const PG_POOL = new Pool({
+  connectionString: TEST_CONNECTION_STRING,
+  application_name: "apoq_test",
+})
+
+export const pgPool = () => PG_POOL
+
+export const withApoq = async (fn: (apoq: Apoq) => Promise<any>) => {
+  const apoqInstance = new Apoq(TEST_CONNECTION_STRING)
+  const result = await fn(apoqInstance)
+  await apoqInstance.stop()
+  return result
+}
+
+export const clearDatabase = async (): Promise<void> => {
+  await pgPool().query(`TRUNCATE TABLE ${DEFAULT_TASK_TABLE} CASCADE`)
+}
+
+beforeAll(async () => {
+  await withApoq((apoq) => apoq.prepare())
+})
+
+afterAll(async () => {
+  await clearDatabase()
+  await PG_POOL.end()
+})
